86b6f0a5c20c373cc9f54b766fdf1f8b6311f8c7
Chiwan Park <chiwanpark@icloud.com>
2013-12-31 01:35:36 +0900

    re-indent whole code

--- src/test/java/com/chiwanpark/flume/plugins/RedisSubscribeDrivenSourceTest.java
+++ src/test/java/com/chiwanpark/flume/plugins/RedisSubscribeDrivenSourceTest.java
@@ -1,6 +1,7 @@
 package com.chiwanpark.flume.plugins;
 
 import com.google.common.collect.Lists;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.apache.flume.*;
@@ -15,71 +16,72 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import redis.clients.jedis.Jedis;
 
 @RunWith(JUnit4.class)
 public class RedisSubscribeDrivenSourceTest {
 
-    private Logger logger = LoggerFactory.getLogger(RedisSubscribeDrivenSourceTest.class);
-    private Context context = new Context();
-    private Channel channel = new MemoryChannel();
-    private ChannelSelector channelSelector = new ReplicatingChannelSelector();
-    private AbstractSource source;
+  private Logger logger = LoggerFactory.getLogger(RedisSubscribeDrivenSourceTest.class);
+  private Context context = new Context();
+  private Channel channel = new MemoryChannel();
+  private ChannelSelector channelSelector = new ReplicatingChannelSelector();
+  private AbstractSource source;
 
-    @Before
-    public void setUp() throws Exception {
-        context.clear();
-        context.put("redisChannel", "flume-ng-redis-test");
+  @Before
+  public void setUp() throws Exception {
+    context.clear();
+    context.put("redisChannel", "flume-ng-redis-test");
 
-        Configurables.configure(channel, context);
-        channelSelector.setChannels(Lists.newArrayList(channel));
+    Configurables.configure(channel, context);
+    channelSelector.setChannels(Lists.newArrayList(channel));
 
-        source = new RedisSubscribeDrivenSource();
-        source.setChannelProcessor(new ChannelProcessor(channelSelector));
-        Configurables.configure(source, context);
+    source = new RedisSubscribeDrivenSource();
+    source.setChannelProcessor(new ChannelProcessor(channelSelector));
+    Configurables.configure(source, context);
 
-        logger.info("Try to start RedisSubscribeDrivenSource.");
-        source.start();
-        Thread.sleep(1000);
+    logger.info("Try to start RedisSubscribeDrivenSource.");
+    source.start();
+    Thread.sleep(1000);
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    source.stop();
+  }
+
+  @Test
+  public void testSubscribe() throws Exception {
+    String message = "testSubscribeMessage!";
+
+    logger.info("Try to send message to redis source.");
+    sendMessageToRedis("flume-ng-redis-test", message);
+    Thread.sleep(2000);
+
+    Assert.assertEquals(message, getMessageFromChannel());
+  }
+
+  private void sendMessageToRedis(String channel, String message) throws Exception {
+    Jedis jedis = new Jedis("localhost", 6379);
+
+    jedis.publish(channel, message);
+  }
+
+  private String getMessageFromChannel() throws Exception {
+    Transaction transaction = channel.getTransaction();
+    try {
+      transaction.begin();
+
+      Event event;
+
+      do {
+        event = channel.take();
+      } while (event == null);
+      transaction.commit();
+
+      return new String(event.getBody(), "UTF-8");
+    } finally {
+      transaction.close();
     }
-
-    @After
-    public void tearDown() throws Exception {
-        source.stop();
-    }
-
-    @Test
-    public void testSubscribe() throws Exception {
-        String message = "testSubscribeMessage!";
-
-        logger.info("Try to send message to redis source.");
-        sendMessageToRedis("flume-ng-redis-test", message);
-        Thread.sleep(2000);
-
-        Assert.assertEquals(message, getMessageFromChannel());
-    }
-
-    private void sendMessageToRedis(String channel, String message) throws Exception {
-        Jedis jedis = new Jedis("localhost", 6379);
-
-        jedis.publish(channel, message);
-    }
-
-    private String getMessageFromChannel() throws Exception {
-        Transaction transaction = channel.getTransaction();
-        try {
-            transaction.begin();
-
-            Event event;
-
-            do {
-                event = channel.take();
-            } while (event == null);
-            transaction.commit();
-
-            return new String(event.getBody(), "UTF-8");
-        } finally {
-            transaction.close();
-        }
-    }
+  }
 }
